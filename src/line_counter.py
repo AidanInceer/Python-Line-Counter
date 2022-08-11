5e0c5f4170f796af5cde73c18804ef68ea7811ef
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-11 20:38:50 +0100

    Read config from config.ini

--- src/line_counter.py
+++ src/line_counter.py
@@ -1,8 +1,17 @@
+import configparser
+import os
+
 from counter import Counter
 from args import get_arguments
+from tree import print_tree
 
-direcorties_to_ignore = ['.git', '.next', 'node_modules', 'site-packages', '__pycache__']
-types_to_ignore = ['.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif', '.svg']
+def get_config():
+    config = configparser.ConfigParser()
+    config.read(config.read(os.path.join(os.path.dirname(__file__), 'config.ini')))
+    directories_to_ignore = config.get('MAIN', 'directories_to_ignore').replace(" ", "").split(',')
+    types_to_ignore = config.get('MAIN', 'types_to_ignore').replace(" ", "").split(',')
+    return directories_to_ignore, types_to_ignore
+
 
 def get_directory_to_scan(args):
     if args.directory is None:
@@ -10,14 +19,18 @@ def get_directory_to_scan(args):
 
     return args.directory
 
+
 def log(message):
     print(message)
 
 
 if __name__ == '__main__':
     args = get_arguments()
+
+    directories_to_ignore, types_to_ignore = get_config()
+
     directory = get_directory_to_scan(args)
-    counter = Counter(directory, args, direcorties_to_ignore, types_to_ignore)
+    counter = Counter(directory, args, directories_to_ignore, types_to_ignore)
 
     directories_count = counter.count_directories()
     log(f"Number of sub-directories to count {directories_count}")
@@ -25,4 +38,5 @@ if __name__ == '__main__':
     result = counter.count_lines()
     log(f"Number of lines found: {result}\n")
 
+    # print_tree(counter.data)
     input("Press enter to exit...")
