d33635ed9c3f6689dc72bd58effcc8ea3725990b
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-13 13:20:15 +0100

    Improve readability

--- src/counter.py
+++ src/counter.py
@@ -21,8 +21,10 @@ class Counter:
 
     
     def print_tree(self):
-        if 'root' not in self.data:
+        if len(self.data.keys()) == 0:
             print('Tree is empty...')
+        elif 'root' not in self.data:
+            print('Tree has no root...')
         else:
             print_tree(self.data)
 
@@ -32,23 +34,26 @@ class Counter:
         for _, dirs, _ in walk(self.path):
             if not self.args.all:
                 dirs[:] = [d for d in dirs if d not in self.config.directories_to_ignore]
-            for _ in dirs:
-                count += 1
+            count += len(dirs)
         return count 
 
 
     def count_lines(self) -> int:
-        line_count = self._directory_line_count(self.path)
+        line_count = self._count_lines_in_directory(self.path)
         
         if self.args.save: 
-            self.data['line_count'] = line_count
-            self.data['root'] = self.path
-
-            print('Saving to file...')
-            make_tree(self.path, self.data)
-            write_to_file(self.data)
+           self._save_result(line_count)
 
         return line_count
+
+
+    def _save_result(self, line_count):
+        self.data['line_count'] = line_count
+        self.data['root'] = self.path
+
+        print('Saving to file...')
+        make_tree(self.path, self.data)
+        write_to_file(self.data)
 
 
     def count_lines_in_file(self, path:str) -> int:
@@ -59,11 +64,10 @@ class Counter:
 
         if self.args.save: self.data[path] = Item(path, ItemType.FILE, line_count)
 
-
         return line_count
 
 
-    def _directory_line_count(self, dir:str, depth:int = 0, total:int = 0, iteration:int = 0) -> int:
+    def _count_lines_in_directory(self, dir:str, depth:int = 0, total:int = 0, iteration:int = 0) -> int:
         line_count =  sum(
                 map(lambda item: self._count_lines_in_item(join(dir, item), depth + 1, total, iteration), 
                     listdir(dir)
@@ -83,7 +87,6 @@ class Counter:
         if isdir(path):
             return self._count_lines_in_driectory(path, depth, total, iteration)
             
-
         elif isfile(path):
             if self.args.printfile: print(path)
             return self.count_lines_in_file(path)
@@ -100,7 +103,7 @@ class Counter:
 
         if self.args.printdirectory: print(path)
 
-        return self._directory_line_count(path, depth + 1, total, iteration)
+        return self._count_lines_in_directory(path, depth + 1, total, iteration)
 
 
 class ItemType(Enum):
