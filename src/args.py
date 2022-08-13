4b3e3b1d992a4c7d91f8cd05fc7b06f4e7199870
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2022-08-13 13:19:44 +0100

    Add printtree flag

--- src/args.py
+++ src/args.py
@@ -5,6 +5,7 @@ def add_arguments(parser):
     parser.add_argument("-d", "--directory", help="The directory to scan", type=str)
     parser.add_argument("-pf", "--printfile", help="Prints the path of every file", action="store_true")
     parser.add_argument("-pd", "--printdirectory", help="Prints the path of every directory", action="store_true")
+    parser.add_argument("-pt", "--printtree", help="Prints directory tree. --save must also have been used", action="store_true")
     parser.add_argument("-a", "--all", help="Scans all directories, even those in the ingore list", action="store_true")
     parser.add_argument("-s", "--save", help="Save results to file", action="store_true")
 
--- src/line_counter.py
+++ src/line_counter.py
@@ -25,5 +25,7 @@ if __name__ == '__main__':
     result = counter.count_lines()
     log(f"Number of lines found: {result}\n")
 
-    counter.print_tree()
+    if args.save and args.print_tree:
+        counter.print_tree()
+    
     input("Press enter to exit...")
